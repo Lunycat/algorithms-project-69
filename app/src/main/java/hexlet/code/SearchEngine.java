33ddd79ea58321419483cf34d445124d4b4ea2b8
Artem <lunycat@mail.ru>
2024-09-20 14:22:45 +0300

    add exact search

--- app/src/main/java/hexlet/code/SearchEngine.java
+++ app/src/main/java/hexlet/code/SearchEngine.java
@@ -9,8 +9,23 @@ public class SearchEngine {
     public static List<String> search(List<Map<String, String>> documents, String text) {
         List<String> found = new ArrayList<>();
 
-        
+        for (var map : documents) {
+            if (contains(map.get("text"), text)) {
+                found.add(map.get("id"));
+            }
+        }
 
         return found;
+    }
+
+    private static boolean contains(String doc, String text) {
+        String[] arr = doc.split(" ");
+
+        for (String s : arr) {
+            if (s.equals(text)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
